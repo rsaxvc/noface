5b961271b64e2f8a7dd6620453d2033e1a2995f0
Richard Allen <rsaxvc@gmail.com>
2014-04-18 14:23:50 +0000

    Move examples to folder

--- haar.py
+++ haar.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
 
-img = cv2.imread('Crowd.jpg')
+img = cv2.imread('example_faces/Crowd.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
