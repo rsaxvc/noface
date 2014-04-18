7c7f330399963c4710387c05ad6ad720e0c7cc27
Richard Allen <rsaxvc@gmail.com>
2014-04-18 06:32:37 +0000

    Camera tracker combo

---
+++ camera_haar.py
@@ -0,0 +1,33 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+	rval, frame = vc.read()
+	print "camera"
+else:
+	rval = False
+	print "nocamera"
+
+while rval:
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		print "face@",x,y
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+w]
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+	cv2.imshow("preview", frame)
+	rval, frame = vc.read()
+	key = cv2.waitKey(20)
+	if key == 27: # exit on ESC
+		break
+cv2.destroyWindow("preview")
