7b900aa19e401a35ced0fad8aa4c381aba623613
Richard Allen <rsaxvc@gmail.com>
2014-04-18 14:56:55 +0000

    Remove eye tracking

--- camera_haar.py
+++ camera_haar.py
@@ -2,7 +2,6 @@ import cv2
 import time
 
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
@@ -23,11 +22,6 @@ while rval:
 	for (x,y,w,h) in faces:
 		print "face@",x,y
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-		roi_gray = gray[y:y+h, x:x+w]
-		roi_color = frame[y:y+h, x:x+w]
-		eyes = eye_cascade.detectMultiScale(roi_gray)
-		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 	cv2.imshow("preview", frame)
 	rval, frame = vc.read()
